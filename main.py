3a12b712d648db060d02da74f09fb13ed8b3774b
k.lo1 <k.lo1@share.epsb.ca>
2022-12-06 14:07:54 -0700

    wtf

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+# main.py
 """
-title: flask Lessons
-author: kliment lo
-date: december 1, 2022
+title: Flask Lessons
+Author: Judy Zhu
+date-created: 12/1/2022
 """
 import csv
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 # --- GLOBAL VARIABLES --- #
 FILENAME = "flask.csv"
@@ -14,18 +15,26 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    readFile(FILENAME)
+    ALERT = ""
+    HEADER, CONTACTS = readFile(FILENAME)
     if request.form:
         FIRSTNAME = request.form.get("first_name")
         LASTNAME = request.form.get("last_name")
         EMAIL = request.form.get("email")
         if checkContent(EMAIL):
             createContact(FIRSTNAME, LASTNAME, EMAIL)
+            ALERT = "Successfully added a new contact!"
         else:
-            pass
-    return render_template("index.html")
+            ALERT = "A contact with the given email already exists"
+    return render_template("index.html", alert=ALERT, contacts=CONTACTS)
+
+@app.route('/delete/<id>')
+def deleteContactPage(id):
+    deleteContact(id)
+    return redirect('/')
 
 # --- CSV --- #
+
 def readFile(FILENAME):
     '''
     opens out csv vile and erads the contents to an array. IF the file has not been created yet, create it now
@@ -74,5 +83,20 @@ def checkContent(EMAIL):
                 return False
         return True
 
+def deleteContact(EMAIL):
+    '''
+    deletes a contact
+    :param EMAIL:
+    :return:
+    '''
+    NEW_DATA = []
+    with open(FILENAME, newline="") as FILE:
+        READER = csv.reader(FILE)
+        for ROW in READER:
+            if ROW[2] != EMAIL:
+                NEW_DATA.append(ROW)
+    with open(FILENAME, "w", newline="") as FILE:
+        WRITER = csv.writer(FILE)
+        WRITER.writerows(NEW_DATA)
 if __name__ == "__main__":
     app.run(debug=True)
